e5c4f9fcec2fad66e03173503053f04331e8df7b
ShonTechDev <shonta.pierce@gmail.com>
2026-03-10 14:09:56 -0400

    updates/deleting commented-out code to increase/maintain code clarity

--- js/index.js
+++ js/index.js
@@ -9,17 +9,6 @@ create a website
     - https://api.artic.edu/api/v1/artists
 */
 
-// fetch('https://api.artic.edu/api/v1/artworks')
-//     .then(response => response.json()) //.then sends a request, then we get access to the response / returns a promise 
-//     .then(data => {
-//         console.log(data); //gets actual data in normal js format
-//     })
-//     .catch(error => {
-//         console.error("Error fetching data:", error);
-//     });
-
-// ^ prints the ENTIRE RESPONSE object to the call; 1st step to ensure I can bring in and inspect the full response
-// ============================================================
 // Adding buttons via id's in html
 const artBtn = document.querySelector("#get-art");
 // const artistBtn = document.querySelector("#get-artist"); removed extra "get artist button from html, removed here in js^^
@@ -37,45 +26,7 @@ const gridRows = document.querySelectorAll(".art-flex-box div");
 const randomMoreBtn = document.querySelector("#get-random-more");
 randomMoreBtn.addEventListener("click", fetchRandomArtWork);
 
-// ============================================================
 
-
-// ======= 1st artwork Fetch art & artist =================
-
-//fetch for Get Most Recent Art button functionality
-
-// function fetchArtWork() { 
-//     fetch('https://api.artic.edu/api/v1/artworks')
-//     .then(response => response.json()) //.then sends a request, then we get access to the response / returns a promise 
-//     .then(data => {
-//         // console.log(data); //gets all data in normal js format
-//         console.log(data.data[0].title); //datapoint 1
-//         console.log(data.data[0].artist_display) //datapoint 2
-//         console.log(data.data[0].image_id) //datapoint 3 //image of artwork
-//         //console.logs displayed in console for me to check
-//         //variables created to work with and connect to my website
-
-//         const artwork = data.data[0]; //first artwork
-//         const artTitle = artwork.title;
-//         const artArtist = artwork.artist_display;
-//         const imageId = artwork.image_id;
-
-
-//         //artwork image url building from the image url equation
-
-//         const imageURL = `https://www.artic.edu/iiif/2/${imageId}/full/843,/0/default.jpg`;
-
-//         //display art on page
-
-//         title.textContent = artTitle;
-//         artist.textContent = artArtist;
-//         image.src = imageURL;
-
-//     })
-//     .catch(error => {
-//         console.error("Error fetching data:", error);
-//     });
-// }
 
 // ======= Most Recent 3 artworks Fetch art & artist =================
 
@@ -107,14 +58,11 @@ randomMoreBtn.addEventListener("click", fetchRandomArtWork);
             });
         }
 
-    // ================================================ 2nd Fetch ====================randomizer===============================
+// ======== 2nd Fetch======randomizer===============
 
-    //adding 2nd fetch hw 
-    //implementing math.floor use my data.data[0] indexting to select a new piece and display it randomly as if walking through the gallery
+//implementing math.floor use my data.data[0] indexting to select a new piece and display it randomly as if walking through the gallery
 
 //'https://api.artic.edu/api/v1/artworks' original api for 1-12 artworks on 1st page
-
-    //random art
 
     function fetchRandomArtWork() {
         //second fetch / click navigation / = Art Admiration Zone
