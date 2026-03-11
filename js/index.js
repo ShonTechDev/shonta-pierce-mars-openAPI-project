ef47194f063694c1567a55ccb8204fcf5e7b18d7
ShonTechDev <shonta.pierce@gmail.com>
2026-03-10 23:31:42 -0400

    updates css html

--- js/index.js
+++ js/index.js
@@ -139,3 +139,21 @@ function fetchArtist() {
     artBtn.addEventListener("click", fetchArtWork);
     randomBtn.addEventListener("click", fetchRandomArtWork);
     artistBtn.addEventListener("click", fetchArtist); 
+
+    //main section play buttons
+
+    document.addEventListener("DOMContentLoaded", function() {
+    const video = document.getElementById("background-video");
+    const playBtn = document.getElementById("play-btn");
+    const pauseBtn = document.getElementById("pause-btn");
+
+    playBtn.addEventListener("click", function() {
+        video.play();
+    });
+
+    pauseBtn.addEventListener("click", function() {
+        video.pause();
+    });
+});
+
+
